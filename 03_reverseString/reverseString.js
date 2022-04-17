23ebcd03a37b46563ce493eec59903c359cf89c4
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-17 16:04:58 +0530

    add solution for reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let ans="";
+    let stringArr = string.split("")
+for(let i = string.length -1 ; i>=0; i--){
+    ans+=stringArr[i];
+}
+return ans;
 
 };
 
