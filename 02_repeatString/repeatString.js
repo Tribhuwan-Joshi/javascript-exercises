2bd87463a54d65576a04e99a12edd3c74b48b1e1
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-17 15:51:28 +0530

    solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if(num<0) return "ERROR";
+    let ans = "";
+    for(let i =0; i<num; i++){
+        ans+=string;
+    }
+    return ans;
 
 };
 
