726afbab778f4a68000b863500029e93af7b281d
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-18 22:39:27 +0530

    add solution for sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start , end) {
+    
+    if(start>end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    if(typeof start === "number" && typeof end ==="number" && start>=0){
+let ans = 0;
+for(let i =start; i<=end; i++){
+    ans+=i;
+}
+return ans;
+}
+else return "ERROR";
 };
+x=sumAll(1,4000);
+console.log(x);
 
 // Do not edit below this line
 module.exports = sumAll;
