37ed42c0b17ee7f4f1894fa08afa74c1727c8af6
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-18 22:48:00 +0530

    minor change

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,8 @@
 const removeFromArray = function(arr,...toBeRemoved) {
 let removedArr= [...toBeRemoved];
 
-for(let i=0; i<arr.length; i++){
-    for(let j = 0; j <removedArr.length ; j++){
-        if(arr[i]===removedArr[j]){
-            arr.splice(i,1);
-            i--;
-            break;
 
-        }
-    }
-    
-}
-return arr;
+return arr.filter(val=>!toBeRemoved.includes(val));
 
 
 };
