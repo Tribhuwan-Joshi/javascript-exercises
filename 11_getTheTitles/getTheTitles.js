2608eca2a1b1537eb84945544b7f173f95fc54e9
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-28 15:26:06 +0530

    add solution for getTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let ans =[];
+    for(let obj of arr){
+        ans.push(obj.title);
+    }
+    return ans;
 
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
