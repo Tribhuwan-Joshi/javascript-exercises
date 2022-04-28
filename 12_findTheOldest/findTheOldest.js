f2485e611c03b48f2dc3acf4dae081b36ca29a92
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-28 16:23:07 +0530

    add solution for findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for(let obj of people){
+        if(!("yearOfDeath" in obj)){
+            obj.yearOfDeath = new Date().getFullYear();
+        }
+        
+        
+    }
+    let oldest = people.sort((a,b)=>(a.yearOfDeath-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)?-1:1);
+    return oldest[0];
 
 };
 
