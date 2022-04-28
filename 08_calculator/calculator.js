964c7e9e8ed7cf33b065310f6b997057af8088cf
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-28 14:33:50 +0530

    add solution for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+  return  a+b;
+  
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return  a-b;
+};
+
+const sum = function(numbers) {
+  return  numbers.reduce(function(sum,val){
+    return sum+=Number(val);
+  },0);
+};
+
+const multiply = function(numbers) {
+    return  numbers.reduce(function(first,next){
+      return first*next;
+    });
+};
+
+const power = function(a,b) {
+  let res = 1;
+  for(let i = 0; i<b;i++){
+    res*=a;
+  }
+  return res;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+	let res = 1;
+  while(x){
+    res*=x;
+    x--;
+    
+  }
+  return res;
 };
 
 // Do not edit below this line
