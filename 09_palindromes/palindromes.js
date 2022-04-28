f4e1517c371448bbb0f1b0d1730cf60a83bc4f08
Tribhuwan Joshi <mathincosmos@gmail.com>
2022-04-28 15:06:02 +0530

    add soltion for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let len = string.length;
+    string = string.toLowerCase();
+    let start = 0;
+    let end = len-1;
+    while(start<=end){
+        let asciiStart = string.charCodeAt(start);
+        let asciiEnd = string.charCodeAt(end);
+        if(asciiStart<97 || asciiStart>122){
+            start++;
+            continue;
+        }
+        else if(asciiEnd<97 || asciiEnd>122){
+            end--;
+            continue;
+        }
+        if(string[start]!==string[end]){
+            return false;
+        }
+        else{
+            start++,end--;
+        }
+    }
+    return true;
+    
 
 };
 
